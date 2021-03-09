846f233b3b079e1a45025005b8d3b7298c898670
Markus Mannheim <markus@mannheim.media>
2021-03-09 15:21:44 +1100

    Add files via upload

--- resources/scrape.py
+++ resources/scrape.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # load in existing data
-data = pd.read_csv("./vaccinations_data.csv")
+data = pd.read_csv("./vaccination_data.csv")
 data.date = pd.to_datetime(data.date)
 last_date = data.iat[len(data) - 1, 0]
 
@@ -84,7 +84,7 @@ if date > last_date:
     
     # save to file    
     data.iloc[len(data)] = [date, vaccinations]
-    data.to_csv("data.csv", index=False)
+    data.to_csv("vaccination_data.csv", index=False)
     print("New data saved to file.")
     
 elif date == last_date:
